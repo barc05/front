85197844b4be8ed224cb0d2bdd84bbc86ec8d975
barc05 <benjaminarucutipa@gmail.com>
2026-05-04 11:28:35 -0400

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Container } from 'react-bootstrap';
+
+export default function Home() {
+    return (
+        <Container className="my-5">
+            <h1>Quiénes Somos</h1>
+            <p>Bienvenido a nuestra Pagina. Esta es la página pública de información.</p>
+        </Container>
+    );
+}
