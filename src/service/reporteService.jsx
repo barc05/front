cd1c9d318326361870793f2ccb8a9d9f3c117aaf
Cri008d <cri.zunigam@duocuc.cl>
2026-05-10 23:55:13 -0400

    Add files via upload

---
+++ src/service/reporteService.jsx
@@ -0,0 +1,21 @@
+const API_URL = "http://localhost:8080/api/reportes";
+
+export const enviarReporteIncendio = async (reporte) => {
+    const datosParaEnviar = {
+        latitud: parseFloat(reporte.latitud),
+        longitud: parseFloat(reporte.longitud),
+        tipoIncendio: reporte.tipoIncendio
+    };
+
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(datosParaEnviar),
+    });
+
+    if (!response.ok) {
+        throw new Error("Error al guardar el reporte");
+    }
+
+    return await response.json();
+};
