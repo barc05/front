8e202ad5eec4e4b458fcd4a3a72eebdd1385a64a
Cri008d <cri.zunigam@duocuc.cl>
2026-05-10 23:57:02 -0400

    Add routing and login page to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import Home from './pages/Home'
-import 'bootstrap/dist/css/bootstrap.min.css' // Asegura que los estilos de Bootstrap carguen
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login'; 
 import NavigationBar from './components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <>
-      <NavigationBar />
-      <Home />
-    </>
+    <Router>
+      <div className="App">
+        <NavigationBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
