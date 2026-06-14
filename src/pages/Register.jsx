05f7e1275b36dc7b830d6b60fc197cda25574944
barc05 <benjaminarucutipa@gmail.com>
2026-06-14 13:58:39 -0400

    register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { Container, Form, Button, Alert, Card } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 
-// Al agregar "export default function", arreglas el error de la consola
 export default function Register() {
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
@@ -14,32 +13,43 @@ export default function Register() {
         e.preventDefault();
         setMensaje({ texto: '', tipo: '' });
 
+
         if (!username.trim() || !email.trim() || !password.trim()) {
             setMensaje({ texto: 'Por favor, completa todos los campos.', tipo: 'warning' });
             return;
         }
 
-        
         try {
-            // Mandamos la petición al Gateway
-            const response = await fetch('https://api-gateway-1w1b.onrender.com/api/v1/usuarios/login', { 
+            
+            const response = await fetch('https://api-gateway-1w1b.onrender.com/api/v1/usuarios/registrar', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ 
-                correo: email,        
-                contraseña: password  
+                    nombre: username,      
+                    correo: email,         
+                    contraseña: password   
                 }),
             });
+
+            
             if (response.ok) {
-                const data = await response.json();
-                localStorage.setItem('token', data.token);
-    
-                setMensaje({ texto: "¡Inicio de sesión correcto!", tipo: "success" });
-                setTimeout(() => navigate('/'), 1500);
+                setMensaje({ texto: '¡Usuario registrado con éxito! Redirigiendo al Login...', tipo: 'success' });
+                
+                
+                setUsername('');
+                setEmail('');
+                setPassword('');
+
+                
+                setTimeout(() => {
+                    navigate('/login');
+                }, 2000);
             } else {
-                setMensaje({ texto: 'Credenciales inválidas. Intenta de nuevo.', tipo: 'danger' });
+               
+                setMensaje({ texto: 'No se pudo completar el registro (Código ' + response.status + ').', tipo: 'danger' });
             }
         } catch (error) {
+           
             setMensaje({ texto: 'Error al conectar con el servidor.', tipo: 'danger' });
         }
     };
@@ -54,17 +64,35 @@ export default function Register() {
                 <Form onSubmit={handleSubmit}>
                     <Form.Group className="mb-3">
                         <Form.Label>Nombre de Usuario</Form.Label>
-                        <Form.Control type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+                        <Form.Control 
+                            type="text" 
+                            placeholder="Ej: ale_arucutipa"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            required
+                        />
                     </Form.Group>
 
                     <Form.Group className="mb-3">
                         <Form.Label>Correo Electrónico</Form.Label>
-                        <Form.Control type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                        <Form.Control 
+                            type="email" 
+                            placeholder="ejemplo@valle.cl"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                        />
                     </Form.Group>
 
                     <Form.Group className="mb-4">
                         <Form.Label>Contraseña</Form.Label>
-                        <Form.Control type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                        <Form.Control 
+                            type="password" 
+                            placeholder="********"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
                     </Form.Group>
 
                     <Button variant="success" type="submit" className="w-100 fw-bold">
