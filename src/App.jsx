28fa093c18723105fa1c83db6a4fce88c76088a6
barc05 <benjaminarucutipa@gmail.com>
2026-06-14 21:31:52 -0400

    error de inmportacion

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import NavigationBar from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Notificaciones from './pages/Notificaciones';
 
 function App() {
   return (
