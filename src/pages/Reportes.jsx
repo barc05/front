fc042fbf1007cb6e4be20357f875c5bcc048c8d8
barc05 <benjaminarucutipa@gmail.com>
2026-07-08 19:23:31 -0400

    correcion import

--- src/pages/Reportes.jsx
+++ src/pages/Reportes.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Container, Spinner, Alert } from 'react-bootstrap';
 import { obtenerReportes } from '../service/reporteService';
-import './Reportes.css';
+import '../style/Reportes.css';
 
 const Reportes = () => {
     const [reportes, setReportes] = useState([]);
